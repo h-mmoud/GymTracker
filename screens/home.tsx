a5a93f491c0773e0dea282bbee50c8329e896a5f
Hassan Mahmoud <hm253@st-andrews.ac.uk>
2024-07-25 17:31:52 +0100

    add date to home

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,11 +1,39 @@
 import * as React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import { format } from 'date-fns';
 
-export default function Home({navigation}) {
+const GetDate = () => {
+  const today = new Date();
+  const formattedDate = format(today, 'PPPP');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.dateText}>{formattedDate}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dateText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  dayText: {
+    fontSize: 20,
+    color: 'gray',
+  },
+});
+
+export default function Home() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button title="Calendar" onPress={() => navigation.navigate("Calendar")} />
+      <GetDate />
     </View>
   );
 }
